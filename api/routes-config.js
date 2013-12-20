60a887b5fa1566c096f022a799b847f3f0db6c2a
Mickey Yawn <mickeyyawn@gmail.com>
2013-12-20 09:36:28 -0500

    cleanup

--- api/routes-config.js
+++ api/routes-config.js
@@ -3,5 +3,5 @@ var events = require('./controller-events');
 module.exports.getEventsRouteConfig = {
   handler: events.getEvents,
   description: 'this is a super awesome route!',
-  notes: 'these are some cool note'
+  notes: 'these are some cool notes'
 };
